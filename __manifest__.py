037ddd60427e53addd98bee6209fbe65e3b70a8d
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-02-22 17:26:37 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hospital Management Test',
+    'version': '1.0.1',
+    'category': 'Hospital',
+    'author': 'Anubhav',
+    'summary': 'System for Hospital Management',
+    'description': 'This is a system for Hospital Management',
+    'depends': [],  # The list of modules that must install before installing the module
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],  # contains all other data files with the module's initial data
+    'demo': [],  # contains data files with demonstration data
+    'installable': True,  # works in odoo 14
+    'auto_install': False,
+    'application': True,  # to include our module in the app list
+    'sequence': -100,  # to bring our module on top of app list
+    'icon': '/om_hospital/static/description/icon.png',
+    'license': 'LGPL-3',
+}
