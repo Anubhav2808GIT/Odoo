7f66250ff0fcd9108311bf617647edc553aefc66
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-02-28 15:59:34 +0530

    Update patient.py

--- models/patient.py
+++ models/patient.py
@@ -6,8 +6,8 @@ class HospitalPatient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]     # both are part of mail module in manifest file-->depends, inherit is used for chatter.
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name' , tracking = True)   #Track visibility is used to track the changes made to the fields, as our system is a multi-user system and the different people can access the same record. 
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    age = fields.Integer(string='Age', tracking = True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking = True)
     active = fields.Boolean(string='Active' default=True)           # gives the Visibility of the records in the view.
