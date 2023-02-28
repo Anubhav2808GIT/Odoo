5855372b8a68c2dbfb876e36054ab5817334b54d
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-02-28 19:26:06 +0530

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -5,3 +5,5 @@ class HospitalAppointment(models.Model):
     _name = "hospital.Appointment"
     _inherit = ["mail.thread", "mail.activity.mixin"]     # both are part of mail module in manifest file-->depends, inherit is used for chatter.
     _description = "Hospital Appointment"
+    
+    patient_id = fields.Many2one('hospital.patient', string = 'Patient')
