52fc0c1143eb0c7f07b6d5e30d0efce5b2ce3774
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-02-28 16:30:06 +0530

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -1,1 +1,7 @@
+from odoo import api, fields, models
 
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.Appointment"
+    _inherit = ["mail.thread", "mail.activity.mixin"]     # both are part of mail module in manifest file-->depends, inherit is used for chatter.
+    _description = "Hospital Appointment"
