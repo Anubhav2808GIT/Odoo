5bc02de0ddbf1158b8b37ef23fbd49dbffca9be6
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-03-01 15:42:45 +0530

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -7,6 +7,6 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
     
     patient_id = fields.Many2one('hospital.patient', string = 'Patient')
-    
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", related="patient_id.gender")
     appointment_time = fields.Datetime(string = 'Appointment Time' , default = fields.Datetime.now)
     booking_date = fields.Date(string = 'Booking Date' , default = fields.Date.context_today)
