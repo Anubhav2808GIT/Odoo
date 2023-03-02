728e29b7ed3242e02a6d2250f5382a1c38c5cfdf
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-03-02 17:32:36 +0530

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -5,6 +5,7 @@ class HospitalAppointment(models.Model):
     _name = "hospital.Appointment"
     _inherit = ["mail.thread", "mail.activity.mixin"]     # both are part of mail module in manifest file-->depends, inherit is used for chatter.
     _description = "Hospital Appointment"
+    _rec_name = "ref"
     
     patient_id = fields.Many2one('hospital.patient', string = 'Patient')
     gender = fields.Selection(related="patient_id.gender")     # we can add another attribute, "read_only=True/False" to make it editable. By default it is True.
