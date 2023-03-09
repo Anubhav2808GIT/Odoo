72dbcc72c096be7b22ea8bdcfec55e79a1a53214
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-03-09 19:02:53 +0530

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -61,9 +61,9 @@ class AppointmentPharmacyLines(models.Model):
     _name = "appointment.pharmacy.lines"
     _description = "Appointment Pharmacy Lines"
     
-    product_id = fields.Many2one('product.product')
-    price_unit = fields.Float(string="Price") 
-    qty = fields.Integer(string="Quantity")
+    product_id = fields.Many2one('product.product', required=True)
+    price_unit = fields.Float(related='product_id.list_price') 
+    qty = fields.Integer(string="Quantity", default=1)
     appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
     
     
