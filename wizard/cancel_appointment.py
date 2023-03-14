04bf63ad4e32ee30d640a7228adf4a0f12dc2ef6
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-03-14 19:39:43 +0530

    Update cancel_appointment.py

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -6,6 +6,7 @@ class CancelAppointmentWizard(models.TransientModel):        #defining a Transie
     _description = "Cancel Appointment Wizard"
     
     appointment_id=fields.Many2one('hospital.appointment', string="Appointment")
+    reason=fields.Text(string="Reason")
     
     def action_cancel(self):
         return
