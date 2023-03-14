6e5e0bcf2b9cd8080f4e5726b2f46ac757da5979
Anubhav Sharma <52752211+Anubhav2808GIT@users.noreply.github.com>
2023-03-14 18:04:26 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -8,8 +8,11 @@
     'depends': ['mail', 'product'],  # The list of modules that must install before installing the module
     'data': [
         'security/ir.model.access.csv',
+        'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/patient_tag_view.xml'
+        'views/female_patient_view.xml',
         'views/appointment_view.xml',
     ],  # contains all other data files with the module's initial data
     'demo': [],  # contains data files with demonstration data
